4d7d786638c784d1a74e829feb7e1d8d40506c04
Francis Barrow <francis.barrow@gmail.com>
2022-09-03 11:45:50 -0400

    complete scrape_category function add urljoin

--- scrape_book.py
+++ scrape_book.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 import re
 import csv
 import helper_table as tab
@@ -56,30 +56,38 @@ def scrape_page(url, csvfile):
     write_book_data(page_content, csvfile)
 
 
-# takes a list of metadata and append a csv file
+# take a list of metadata and append it to a csv file
 def write_book_data(list_of_data, csvfile):
-    # /!\ ISSUE WITH CSV APPEND/WRITE /!\
-    with open(csvfile, 'a', newline=''):
-        writer = csv.writer(csvfile, delimiter=',')
+    with open(csvfile, 'a', newline='') as file:
+        writer = csv.writer(file, delimiter=',')
         writer.writerow(list_of_data)
 
 
+# get links from a category and scrape each book of the category
 def scrape_category():
     url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
 
-    parsed_url = urlparse(url)
-    root_url = parsed_url.scheme + '://' + parsed_url.netloc
+    # gather all books in category page, return list of urls
+    def get_books_urls(page_url, books_urls):
+        page = requests.get(page_url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        new_books_urls = soup.select('.product_pod>div>a')
 
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+        # extract href from soup selection and join them to page_url
+        def join_url(book):
+            return urljoin(page_url, book.attrs.get('href'))
 
-    # gather all books in category page
-    # MISSING: next pages if present
-    books = soup.select('.product_pod>div>a')
-    # modify urls and call scrape_book for each
-    for book in books:
-        book_url = book.attrs.get('href')
-        book_url = book_url.replace('../../..', root_url + '/catalogue')
+        new_books_urls = list(map(join_url, new_books_urls))
+        books_urls = books_urls + new_books_urls
+        # check for next page and call get_books_urls recursively
+        next_page = soup.select_one('.next a')
+        if next_page:
+            next_url = urljoin(page_url, next_page['href'])
+            return get_books_urls(next_url, books_urls)
+        else:
+            return books_urls
+
+    for book_url in get_books_urls(url, []):
         scrape_page(book_url, 'data_category.csv')
 
 
