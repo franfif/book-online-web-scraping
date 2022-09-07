b969114f83c1aba6cd9eb9bc79836bff726731c5
Francis Barrow <francis.barrow@gmail.com>
2022-09-07 11:22:45 -0400

    replace get_value_from_table function with a filter function

--- helper_table.py
+++ helper_table.py
@@ -1,5 +1,3 @@
 def get_value_from_table(soup, header):
-    rows = soup.select('table tr')
-    for row in rows:
-        if row.select_one('th').string == header:
-            return row.select_one('td').string
+    return list(filter(lambda x: x.select_one('th').string == header,
+                       soup.select('table tr')))[0].select_one('td').string
